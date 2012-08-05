f2960973b56f8bfb3105cbbd480fa8c3edc159e2
Brian Scully <scullduggery@gmail.com>
2012-08-04 23:12:31 -0400

    Beefing up specs; direction change on scale, only track digits after decimal

--- spec/fpspec.js
+++ spec/fpspec.js
@@ -1,55 +1,70 @@
 describe("Constructors", function() {
+	it("should support construction by value and scale", function() {
+		var fp = new FP.Decimal(1,2);
+		expect(fp.value()).toBe(1);
+		expect(fp.scale()).toBe(2);
+	});
 	it("should support construction from a string", function() {
 		var fp = new FP.Decimal("0.01");
-		expect(fp.value).toBe(1);
-		expect(fp.scale).toBe(-2);
+		expect(fp.value()).toBe(1);
+		expect(fp.scale()).toBe(2);
 	});
 	it("should support construction from a float", function() {
 		var fp = new FP.Decimal(0.01);
-		expect(fp.value).toBe(1);
-		expect(fp.scale).toBe(-2);
+		expect(fp.value()).toBe(1);
+		expect(fp.scale()).toBe(2);
 	});
 	it("should convert trailing zeros on integers to positive scale", function() {
 		var fp = new FP.Decimal(100);
-		expect(fp.value).toBe(1);
-		expect(fp.scale).toBe(2);
+		expect(fp.value()).toBe(100);
+		expect(fp.scale()).toBe(0);
 
 		var fp = new FP.Decimal(1230);
-		expect(fp.value).toBe(123);
-		expect(fp.scale).toBe(1);
+		expect(fp.value()).toBe(1230);
+		expect(fp.scale()).toBe(0);
+	});
+	it("should convert not consider intervening zeros on integers to be trailing", function() {
+		var fp = new FP.Decimal(10010);
+		expect(fp.value()).toBe(10010);
+		expect(fp.scale()).toBe(0);
 	});
 	it("should fail if a non-numeric string is used", function() {
 		expect(function() { new FP.Decimal("abc"); }).toThrow();
 	});
 	it("should support factory method from a string", function() {
 		var fp = FP.Decimal.valueOf("0.01");
-		expect(fp.value).toBe(1);
-		expect(fp.scale).toBe(-2);
+		expect(fp.value()).toBe(1);
+		expect(fp.scale()).toBe(2);
 	});
 	it("should support factory method from a float", function() {
 		var fp = FP.Decimal.valueOf(0.01);
-		expect(fp.value).toBe(1);
-		expect(fp.scale).toBe(-2);
+		expect(fp.value()).toBe(1);
+		expect(fp.scale()).toBe(2);
 	});
 });
 
-describe("Scale conversion", function() {
-	var fp = new FP.Decimal(1.23);
-	expect(fp.value).toBe(123);
-	expect(fp.scale).toBe(-2);
-	it("should convert both the value and the scale to stay the same for negative", function() {
-		var fp1 = fp.convert(-1);
-		expect(fp1.value).toBe(12.3);
-		expect(fp1.scale).toBe(-1);
+describe("Rounding", function() {
+	var fp = new FP.Decimal(1.253);
+	expect(fp.value()).toBe(1253);
+	expect(fp.scale()).toBe(3);
+	it("should not affect the value if the scale is >= current", function() {
+		var fp1 = fp.scaleTo(4);
+		expect(fp1.value()).toBe(12530);
+		expect(fp1.scale()).toBe(4);
 	});
-	it("should convert both the value and the scale to stay the same for positive", function() {
-		var fp1 = fp.convert(1);
-		expect(fp1.value).toBe(0.123);
-		expect(fp1.scale).toBe(1);
+	it("should truncate the value if the scale is < current and the last digit is less than 5", function() {
+		var fp1 = fp.scaleTo(2);
+		expect(fp1.value()).toBe(125);
+		expect(fp1.scale()).toBe(2);
+	});
+	it("should round the value up if the scale is < current and the last digit is >= 5", function() {
+		var fp1 = fp.scaleTo(1);
+		expect(fp1.value()).toBe(13);
+		expect(fp1.scale()).toBe(1);
 	});
 });
 
-xdescribe("Addition", function() {
+describe("Addition", function() {
 	it("should fail with a TypeError when trying to add something that's not coercable to an FP.Decimal",function() {
 		var fp1 = new FP.Decimal(2);
 		expect(function() { fp1.add("abc"); }).toThrow();
@@ -58,6 +73,24 @@ xdescribe("Addition", function() {
 	it("should add integers correctly", function() {
 		var fp1 = new FP.Decimal(2);
 		var fp2 = new FP.Decimal(2);
-		expect(fp1.add(fp2)).toBe(4);
+		var sum = fp1.add(fp2);
+		expect(sum.value()).toBe(4);
+		expect(sum.scale()).toBe(0);
+	});
+
+	it("should add decimals of same scale correctly", function() {
+		var fp1 = new FP.Decimal(2.1);
+		var fp2 = new FP.Decimal(2.1);
+		var sum = fp1.add(fp2);
+		expect(sum.value()).toBe(42);
+		expect(sum.scale()).toBe(1);
+	});
+
+	it("should add decimals of different scale correctly", function() {
+		var fp1 = new FP.Decimal(2.1);
+		var fp2 = new FP.Decimal(2.11);
+		var sum = fp1.add(fp2);
+		expect(sum.value()).toBe(421);
+		expect(sum.scale()).toBe(2);
 	});
 });
--- src/fp.js
+++ src/fp.js
@@ -1,69 +1,122 @@
 var FP = FP || {};
 
-FP.CheckNumeric = function(value) {
-	if (typeof(value) === "string" && value.search(/[^0-9.]/) > -1)	{
-		throw new TypeError("Strings passed can only include numbers and decimal point");
-	}
-}
+FP.Rounding = {
+	CEILING: { type: "CEILING", op: Math.ceiling },
+	FLOOR: { type: "FLOOR", op: Math.floor },
+	ROUND: { type: "ROUND", op: Math.round }
+};
+
+FP.Rounding.DEFAULT = FP.Rounding.ROUND;
 
 FP.Decimal = function(value) {
 	"use strict";
 	"use restrict";
 
-	var _value = 0,
+	var checkNumeric = function(value) {
+			if (typeof(value) === "string" && value.search(/[^0-9.]/) > -1)	{
+				throw new TypeError("Strings passed can only include numbers and decimal point");
+			}
+		},
+	 	typeTest = function(other) {
+			typeTestProperties(other.value, other.scale);
+		},
+		typeTestProperties = function(otherValue, otherScale) {
+			if (typeof(otherValue) === "undefined" || typeof(otherScale) === "undefined")
+			{
+				throw new TypeError("All operations expects FP.Decimal objects to be passed");
+			}
+		},
+		_value = 0,
 		_scale = 0;
 
-	FP.CheckNumeric(value);
+	checkNumeric(value);
 
-	if (typeof(value) === "number") {
-		value = value.toString();
+	if (arguments.length === 2 && typeof(arguments[0]) === "number" && typeof(arguments[1]) === "number") {
+		_value = arguments[0];
+		_scale = arguments[1];
 	}
-	if (typeof(value) === "string") {
-		if (value.indexOf(".") > 0) {
-			_value = (+value.replace("\.",""));
-			_scale = -(value.length - value.indexOf(".") - 1);
+	else {
+		if (typeof(value) === "number") {
+			value = value.toString();
 		}
-		else if (value.indexOf("0") > 0) {
-			_value = (+value.substring(0,value.indexOf("0")));
-			_scale = value.length - value.indexOf("0");
+		if (typeof(value) === "string") {
+			if (value.indexOf(".") > 0) {
+				_value = (+value.replace("\.",""));
+				_scale = (value.length - value.indexOf(".") - 1);
+			}
+			else {
+				_value = (+value);
+				_scale = 0;
+			}
 		}
 	}
-
-	var typeTest = function(other) {
-		typeTestProperties(other.value, other.scale);
-	};
-
-	var typeTestProperties = function(otherValue, otherScale) {
-		if (typeof(otherValue) === "undefined" || typeof(otherScale) === "undefined")
-		{
-			throw new TypeError("Add expects FP.Decimal objects to be added");
-		}
-	};
 
 	return {
-		value: _value,
+		value: function() { return _value; },
 
-		scale: _scale,
+		scale: function() { return _scale; },
 
-		convert: function(otherScale) {
-			var scaleFactor = this.scale - otherScale;
-			this.value = this.value * Math.pow(10,scaleFactor);
-			this.scale = otherScale;
-			return this;
+		scaleTo: function(newScale, roundingType) {
+			roundingType = roundingType || FP.Rounding.DEFAULT;
+			if (newScale >= _scale) {
+				var addedZeroes = newScale - _scale,
+					extraZeroes = "",
+					str = _value.toString();
+				for(var i=0; i<addedZeroes; i++)
+					extraZeroes += "0";
+				str = str + extraZeroes;
+				return new FP.Decimal((+str),newScale);
+			}
+			else {
+				var truncatedDigits = _scale - newScale,
+					str = _value.toString(),
+					roundingVal = (+str[str.length - truncatedDigits]),
+					newValue = _value;
+				str = str.substring(0,str.length-truncatedDigits);
+
+				if(FP.Rounding.CEILING === roundingType) {
+					newValue = (+str)+1;
+				}
+				else if (FP.Rounding.ROUND === roundingType) {
+					if(roundingVal >= 5) {
+						newValue = (+str)+1;
+					}
+					else {
+						newValue = (+str);
+					}
+				}
+				else {
+					newValue = (+str);
+				}
+
+				return new FP.Decimal(newValue,newScale);
+			}
 		},
 
 		add: function(other) {
-			var otherValue = other.value,
-				otherScale = other.scale;
-			typeTestProperties(otherValue,otherScale);
-			return new FP.Decimal
+			if (typeof(other) === "number" || typeof(other) == "string") {
+				other = new FP.Decimal(other);
+			}
+			typeTest(other);
+
+			var that = this;
+			if (other.scale() > _scale)
+			{
+				that = this.scaleTo(other.scale());
+			}
+			else if (_scale > other.scale())
+			{
+				other = other.scaleTo(_scale);
+			}
+
+			return new FP.Decimal(that.value()+other.value(),that.scale());
 		},
 
 		equals: function(other) {
 			return typeof other !== 'undefined' &&
 			        other !== null &&
-			        this._value === other._value
-			        && this._scale === other._scale
+			        this.value() === other.value() && 
+			        this.scale() === other.scale();
     	},
 
 		toString: function() {
@@ -77,3 +130,5 @@ FP.Decimal = function(value) {
 FP.Decimal.valueOf = function(value) {
 	return new FP.Decimal(value);
 };
+
+
